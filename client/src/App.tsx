b6b40c4a3d5c3a62bb4e35cdc6dc2c064e1021e2
Anjali Negi <113291155+Anjali4402@users.noreply.github.com>
2025-12-16 17:18:59 +0530

    setup api and added procted routes in FE ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,38 @@
-
-import './App.css';
-// import { } from 'react-dom';
-// import { Routes } from "react-router"
-import { Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import HomePage from './pages/HomePage';
-import ProfilePage from './pages/ProfilePage';
-
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
+import { Toaster } from "react-hot-toast";
+import { AuthContext } from "./context/AuthContext";
+import { useContext } from "react";
 
 function App() {
+  const { authUser } = useContext(AuthContext);
 
   return (
     <div
-    className="bg-[url('./assets/bgImage.svg')] bg-contain
+      className="bg-[url('./assets/bgImage.svg')] bg-contain
     bg-no-repeatbg-coverbg-center
     "
     >
-    <Routes>
-      <Route path='/login' element={<LoginPage />} />
-      <Route path='/' element={<HomePage />} />
-      <Route path='/profile' element={<ProfilePage />} />
-    </Routes>
-
+      <Toaster />
+      <Routes>
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/profile"
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
